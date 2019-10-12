f6a237d4ec5bed8e8dd9b94628528ffe68df83c5
Steven Laff <steve.laff@gmail.com>
2019-10-12 19:54:18 -0400

    initial commit

---
+++ lib/animal_expert.rb
@@ -0,0 +1,6 @@
+require "animal_expert/version"
+
+module AnimalExpert
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/animal_expert/version.rb
@@ -0,0 +1,3 @@
+module AnimalExpert
+  VERSION = "0.1.0"
+end
